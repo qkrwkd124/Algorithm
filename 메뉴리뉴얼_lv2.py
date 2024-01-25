8140f991e0bb3eeac8a4c93b63d0a1aebb8c6aec
qkrwkd124 <qkrwkd120@naver.com>
2024-01-25 16:35:18 +0900

    임시

--- 메뉴리뉴얼_lv2.py
+++ 메뉴리뉴얼_lv2.py
@@ -1,5 +1,32 @@
+result = []
+check = []
+course_max_cnt = []
+courses = {}
+
+def combi(start:int, n:int, order:list) :
+    global result 
+    
+    if len(result) == n :
+        
+    else : 
+        for i in range(start,len(order)):
+            
+
+
 def solution(orders, course):
     answer = []
+
+    global check
+
+    # subset = ['A','D']
+    # answer = all(elem in orders[3] for elem in subset)
+    for order in orders :
+        for course_num in course :
+            check = [False] * course_num
+            if len(order) < course_num :
+                continue
+            combi(0,course_num, order)
+    
     return answer
 
 

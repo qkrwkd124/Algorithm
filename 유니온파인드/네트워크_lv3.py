3f51cbdeae88b6e1c1585987fc5ccde330d29661
qkrwkd124 <qkrwkd120@naver.com>
2024-02-25 23:35:59 +0900

    임시

---
+++ 유니온파인드/네트워크_lv3.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+def find(parent:list, x:int) :
+    if parent[x] != x :
+        parent[x] = find(parent,parent[x])
+    
+    return parent[x]
+
+def union(parent:list, a:int, b:int) :
+
+    root_a = find(parent,a)
+    root_b = find(parent,b)
+
+    if root_a <= root_b :
+        parent[root_b] = root_a
+    else :
+        parent[root_a] = root_b
+
+
+def solution(n, computers):
+    answer = 0
+
+    parent = [i for i in range(n)]
+
+    for a in range(n) :
+        for b in range(n) :
+            if a == b :
+                continue
+
+            if computers[a][b] == 1 :
+                union(parent,a,b)
+
+    
+    # print(Counter(parent).values())
+
+    for i in range(n):
+        find(parent, i)
+
+    # print(parent)
+    
+    answer = len(set(parent))
+
+
+    return answer
+
+
+n = 3
+n = 5
+computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+# computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+computers = [[1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0],[0, 0, 0, 1, 1], [0,0,0,1,1]]
+n = 4
+computers= computers = [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 0], [1, 0, 0, 1]]
+
+print(solution(n,computers))

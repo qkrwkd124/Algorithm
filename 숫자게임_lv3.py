007b62bb9be0e303b55467df0072de041ec208b4
qkrwkd124 <qkrwkd120@naver.com>
2024-02-28 00:03:47 +0900

    final

--- 숫자게임_lv3.py
+++ 숫자게임_lv3.py
@@ -6,42 +6,18 @@ def solution(A, B):
     sort_A = sorted(A, reverse=True)
     sort_B = sorted(B, reverse=True)
 
+    A_dq = deque(sort_A)
     B_dq = deque(sort_B)
     temp_dq = deque()
 
-    for A_val in sort_A :
-        index = None
-        for i,B_val in enumerate(sort_B) :
-
-            if B_val == 0 :
-                continue
-            if A_val < B_val :
-                index = i
-            else :
-                break
-        # print(sort_A)
-        # print(sort_B)
-        # print(f"{A_val} - {index}")
-        
-        if index != None:
-            sort_B[index] = 0
-            answer +=1
-        # while B_dq :
-        #     B_val = B_dq.popleft()
-
-        #     if A_val < B_val :
-        #         temp_dq.append(B_val)
-        #     else :
-        #         B_dq.appendleft(B_val)
-        #         break
-
-        # print(f"{A_val} - {temp_dq}")
-
-        # if temp_dq :
-        #     temp_dq.pop()
-        #     answer +=1
-        #     while temp_dq :
-        #         B_dq.appendleft(temp_dq.pop())
+    while B_dq :
+        if A_dq[0] < B_dq[0] :
+            A_dq.popleft()
+            B_dq.popleft()
+            answer+=1
+        else :
+            A_dq.popleft()
+            B_dq.pop()
 
     return answer
 
